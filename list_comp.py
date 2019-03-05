6b1ddc20dda239bc6532f6b91ec96cb44b45802a
Christopher Motes <christopher@flux7.com>
2019-03-05 13:01:24 -0600

    avoid hidden files

--- list_comp.py
+++ list_comp.py
@@ -1,4 +1,4 @@
 import re
 import os
-fileNames = [ re.sub('\..*$', '', el) + '.txt' for el in os.listdir('.') ]
+fileNames = [ re.sub('\..*$', '', el) + '.txt' for el in os.listdir('.') if not re.match("^\.", el) ]
 print(fileNames)
