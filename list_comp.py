fe858a2892323c96e98fe1bb00e6ca33728eaef4
Christopher Motes <christopher@flux7.com>
2019-03-05 13:14:39 -0600

    add move line

--- list_comp.py
+++ list_comp.py
@@ -1,8 +1,8 @@
 import re
 import os
-fileNames = [ 
-                re.sub('\..*$', '', el) + '.txt' 
+print( [ 
+                'mv ' + el + ' ' + re.sub('\..*$', '', el) + '.txt' 
                 for el in os.listdir('.') 
                 if not re.match("^\.", el) 
             ]
-print(fileNames)
+)
