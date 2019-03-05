563a7e6eff3d4ed728790f66733774e92dae8230
Christopher Motes <christopher@flux7.com>
2019-03-05 13:08:31 -0600

    look pretty

--- list_comp.py
+++ list_comp.py
@@ -1,4 +1,8 @@
 import re
 import os
-fileNames = [ re.sub('\..*$', '', el) + '.txt' for el in os.listdir('.') if not re.match("^\.", el) ]
+fileNames = [ 
+                re.sub('\..*$', '', el) + '.txt' 
+                for el in os.listdir('.') 
+                if not re.match("^\.", el) 
+            ]
 print(fileNames)
